5a5a9fc9f8913fc05c6701842257fce8b157264b
Ignacio <imoon521@gmail.com>
2019-01-20 14:43:16 -0600

    Refactor of code to follow new folder strategy

---
+++ api/controllers/auth.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const Auth = require("../models/Auth");
+const test = (req, res) => {
+  res.status(200).json({
+    message: "Auth Route Works"
+  });
+};
+
+// @route  POST api/auth/signup
+// @desc   Signup user with email and password
+// @access Public
+const signup = (req, res) => {
+  const auth = new Auth();
+  auth.signup(req.body).then(response => {
+    const { isValid, user, errors } = response;
+    if (isValid) {
+      return res.status(201).json({ user });
+    } else {
+      return res.status(400).json({ errors });
+    }
+  });
+};
+
+// @route  POST api/auth/signin
+// @desc   Signin user with email and password
+// @access Public
+const signin = (req, res) => {
+  const auth = new Auth();
+  auth.signin(req.body).then(response => {
+    const { isValid, tokens, errors, status } = response;
+    if (isValid) {
+      return res.status(status).json({ ...tokens });
+    } else {
+      return res.status(status).json({ errors });
+    }
+  });
+};
+
+module.exports = {
+  test,
+  signup,
+  signin
+};
